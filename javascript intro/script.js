b28e978ca814d4cc1cd9456775c8f476a6c2a2d4
Uchenna Alua <81146129+uchealua@users.noreply.github.com>
2022-06-23 15:49:52 +0100

    Add files via upload

---
+++ javascript intro/script.js
@@ -0,0 +1,15 @@
+function myFunction(){
+    var name =prompt("Please enter your name","Uchenna Alua");
+    var height = prompt("Please enter your height:", "6ft 10in");
+    var country = prompt("Please enter your country:", "Nigeria");
+if (name != null && name != "")
+if (height != null && height != "")
+if (country != null && country !="") {
+    document.write ("My Name is " + name + ". I am " + height + ". I live in " + country);
+  }
+document.getElementById("commandPrompt").innerHTML = text;
+}
+
+
+
+
